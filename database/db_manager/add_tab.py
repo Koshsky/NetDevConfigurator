b407143d230091581dafb55b0c8e000cfb22db73
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-11-30 00:22:46 +0300

    new names for widgets in app_tab (not enough good)

--- database/db_manager/add_tab.py
+++ database/db_manager/add_tab.py
@@ -11,62 +11,60 @@ class AddTab:
 
     def create_widgets(self):
         # Первая строка
-        self.label1 = ttk.Label(self.frame, text="company")
-        self.label1.grid(row=0, column=0, padx=5, pady=5)
+        self.entity_1 = ttk.Label(self.frame, text="company")
+        self.entity_1.grid(row=0, column=0, padx=5, pady=5)
 
-        self.name1 = ttk.Label(self.frame, text="name:")
-        self.name1.grid(row=0, column=1, padx=5, pady=5)
+        self.param_1_1 = ttk.Label(self.frame, text="name:")
+        self.param_1_1.grid(row=0, column=1, padx=5, pady=5)
+        self.field_1_1 = ttk.Entry(self.frame)
+        self.field_1_1.grid(row=0, column=2, padx=5, pady=5)
 
-        self.field1 = ttk.Entry(self.frame)
-        self.field1.grid(row=0, column=2, padx=5, pady=5)
-
-        self.button1 = tk.Button(self.frame, text="SUBMIT", command=lambda: self.on_button_click("SUBMIT 1"))
-        self.button1.grid(row=0, column=3, padx=5, pady=5)
+        self.button_1 = tk.Button(self.frame, text="SUBMIT", command=lambda: self.on_button_click("SUBMIT 1"))
+        self.button_1.grid(row=0, column=3, padx=5, pady=5)
 
         # Вторая строка
-        self.label3 = ttk.Label(self.frame, text="firmware")
-        self.label3.grid(row=1, column=0, padx=5, pady=5)
+        self.entity_2 = ttk.Label(self.frame, text="firmware")
+        self.entity_2.grid(row=1, column=0, padx=5, pady=5)
 
-        self.name2 = ttk.Label(self.frame, text="name:")
-        self.name2.grid(row=1, column=1, padx=5, pady=5)
+        self.param_2_1 = ttk.Label(self.frame, text="name:")
+        self.param_2_1.grid(row=1, column=1, padx=5, pady=5)
+        self.field_2_1 = ttk.Entry(self.frame)
+        self.field_2_1.grid(row=1, column=2, padx=5, pady=5)
 
-        self.field2 = ttk.Entry(self.frame)
-        self.field2.grid(row=1, column=2, padx=5, pady=5)
-
-        self.button2 = tk.Button(self.frame, text="SUBMIT", command=lambda: self.on_button_click("SUBMIT 2"))
-        self.button2.grid(row=1, column=3, padx=5, pady=5)
+        self.button_2 = tk.Button(self.frame, text="SUBMIT", command=lambda: self.on_button_click("SUBMIT 2"))
+        self.button_2.grid(row=1, column=3, padx=5, pady=5)
 
         # третий блок
-        self.device_label = ttk.Label(self.frame, text="device")
-        self.device_label.grid(row=2, column=0, padx=5, pady=5)
+        self.entity_3 = ttk.Label(self.frame, text="device")
+        self.entity_3.grid(row=2, column=0, padx=5, pady=5)
 
-        self.name3 = ttk.Label(self.frame, text="name")
-        self.name3.grid(row=2, column=1, padx=5, pady=5)
-        self.field2 = ttk.Entry(self.frame)
-        self.field2.grid(row=2, column=2, padx=5, pady=5)
+        self.param_3_1 = ttk.Label(self.frame, text="name")
+        self.param_3_1.grid(row=2, column=1, padx=5, pady=5)
+        self.field_3_1 = ttk.Entry(self.frame)
+        self.field_3_1.grid(row=2, column=2, padx=5, pady=5)
 
-        self.name23 = ttk.Label(self.frame, text="company")
-        self.name23.grid(row=3, column=1, padx=5, pady=5)
-        self.field22 = ttk.Entry(self.frame)
-        self.field22.grid(row=3, column=2, padx=5, pady=5)
+        self.param_3_2 = ttk.Label(self.frame, text="company")
+        self.param_3_2.grid(row=3, column=1, padx=5, pady=5)
+        self.field_3_2 = ttk.Entry(self.frame)
+        self.field_3_2.grid(row=3, column=2, padx=5, pady=5)
 
-        self.name13 = ttk.Label(self.frame, text="dev_type")
-        self.name13.grid(row=4, column=1, padx=5, pady=5)
-        self.field21 = ttk.Entry(self.frame)
-        self.field21.grid(row=4, column=2, padx=5, pady=5)
+        self.param_3_3 = ttk.Label(self.frame, text="dev_type")
+        self.param_3_3.grid(row=4, column=1, padx=5, pady=5)
+        self.field_3_3 = ttk.Entry(self.frame)
+        self.field_3_3.grid(row=4, column=2, padx=5, pady=5)
 
-        self.name321 = ttk.Label(self.frame, text="prim_conf")
-        self.name321.grid(row=5, column=1, padx=5, pady=5)
-        self.field212 = ttk.Entry(self.frame)
-        self.field212.grid(row=5, column=2, padx=5, pady=5)
+        self.param_3_4 = ttk.Label(self.frame, text="prim_conf")
+        self.param_3_4.grid(row=5, column=1, padx=5, pady=5)
+        self.field_3_4 = ttk.Entry(self.frame)
+        self.field_3_4.grid(row=5, column=2, padx=5, pady=5)
 
-        self.name33 = ttk.Label(self.frame, text="port_num")
-        self.name33.grid(row=6, column=1, padx=5, pady=5)
-        self.field122 = ttk.Entry(self.frame)
-        self.field122.grid(row=6, column=2, padx=5, pady=5)
+        self.param_3_5 = ttk.Label(self.frame, text="port_num")
+        self.param_3_5.grid(row=6, column=1, padx=5, pady=5)
+        self.field_3_5 = ttk.Entry(self.frame)
+        self.field_3_5.grid(row=6, column=2, padx=5, pady=5)
 
-        self.button234 = tk.Button(self.frame, text="SUBMIT", command=lambda: self.on_button_click("SUBMIT 3"))
-        self.button234.grid(row=6, column=3, padx=5, pady=5)
+        self.button_3 = tk.Button(self.frame, text="SUBMIT", command=lambda: self.on_button_click("SUBMIT 3"))
+        self.button_3.grid(row=6, column=3, padx=5, pady=5)
 
     def on_button_click(self, button_name):
         print(f"{button_name} clicked")
