268bad530e8664bfab1939a3f0fe1644ca9f10a8
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-05 23:59:44 +0300

    убрал простыни в DatabaseApp

--- database/app.py
+++ database/app.py
@@ -42,6 +42,7 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 class DatabaseApp:
     def __init__(self, root):
         self.init_root(root)
+        self.tabs = []
         
         self.company_service = None
         self.firmware_service = None
@@ -66,48 +67,30 @@ class DatabaseApp:
 
         self.connection_tab = ConnectionTab(self.notebook, self.on_success_callback, self.on_failure_callback, self)
         self.notebook.add(self.connection_tab.frame, text="Connection")
-
-        self.tables_tab = TablesTab(self.notebook, self)
-        self.notebook.add(self.tables_tab.frame, text="Tables")
-
-        self.company_info_tab = CompanyInfoTab(self.notebook, self)
-        self.notebook.add(self.company_info_tab.frame, text="Company info")
-
-        self.device_info_tab = DeviceInfoTab(self.notebook, self)
-        self.notebook.add(self.device_info_tab.frame, text="Device info")
-
-        self.firmware_info_tab = FirmwareInfoTab(self.notebook, self)
-        self.notebook.add(self.firmware_info_tab.frame, text="Firmware info")
-
-        self.add_tab = AddTab(self.notebook, self)
-        self.notebook.add(self.add_tab.frame, text="Add")
-
-        self.update_tab = UpdateTab(self.notebook, self)
-        self.notebook.add(self.update_tab.frame, text="Update")
-
-        self.delete_tab = DeleteTab(self.notebook, self)
-        self.notebook.add(self.delete_tab.frame, text="Delete")
+        
+        self.create_tab(TablesTab, "Tables")
+        self.create_tab(CompanyInfoTab, "Company_info")
+        self.create_tab(DeviceInfoTab, "Device info")
+        self.create_tab(FirmwareInfoTab, "Firmware info")
+        self.create_tab(AddTab, "Add")
+        self.create_tab(UpdateTab, "Update")
+        self.create_tab(DeleteTab, "Delete")
 
         self.hide_all_tabs()
+        
+    def create_tab(self, ClassTab: type, tab_name: str):
+        tab = ClassTab(self.notebook, self)
+        self.notebook.add(tab.frame, text=tab_name)
+        self.tabs.append(tab)
 
     def display_all_tabs(self):
-        self.notebook.select(self.tables_tab.frame)
-        self.notebook.select(self.company_info_tab.frame)
-        self.notebook.select(self.device_info_tab.frame)
-        self.notebook.select(self.firmware_info_tab.frame)
-        self.notebook.select(self.add_tab.frame)
-        self.notebook.select(self.update_tab.frame)
-        self.notebook.select(self.delete_tab.frame)
+        for tab in self.tabs:
+            self.notebook.select(tab.frame)
         self.notebook.select(self.connection_tab.frame)  # чтобы не изменять активную вкладку
 
     def hide_all_tabs(self):
-        self.notebook.hide(self.tables_tab.frame)
-        self.notebook.hide(self.company_info_tab.frame)
-        self.notebook.hide(self.device_info_tab.frame)
-        self.notebook.hide(self.firmware_info_tab.frame)
-        self.notebook.hide(self.add_tab.frame)
-        self.notebook.hide(self.update_tab.frame)
-        self.notebook.hide(self.delete_tab.frame)
+        for tab in self.tabs:
+            self.notebook.hide(tab.frame)
 
     def on_success_callback(self, engine):
         self.Session = sessionmaker(bind=engine)
