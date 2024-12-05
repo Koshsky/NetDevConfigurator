d0a000d7657477db887548b61d4bf7e4748a1e04
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-05 21:17:37 +0300

    new db services: protocols device_protocols

---
+++ database/services/device_protocols_service.py
@@ -0,0 +1,54 @@
+from sqlalchemy.orm import Session
+
+from database.models.models import DeviceProtocols, Devices, Protocols
+
+
+class DeviceProtocolsService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self):
+        return self.db.query(DeviceProtocols).all()
+
+    def get_by_id(self, device_protocol_id: int):
+        return self.db.query(DeviceProtocols).filter(DeviceProtocols.id == device_protocol_id).first()
+
+    def create(self, device_protocol: DeviceProtocols):
+        self.db.add(device_protocol)
+        self.db.commit()
+        self.db.refresh(device_protocol)
+        return device_protocol
+
+    def delete(self, device_protocol: DeviceProtocols):
+        if device_protocol:
+            self.db.delete(device_protocol)
+            self.db.commit()
+
+    def delete_by_id(self, device_protocol_id: int):
+        device_protocol = self.get_by_id(device_protocol_id)
+        self.delete(device_protocol)
+        
+    def link(self, device_id: int, protocol_id: int):
+        existing_link = self.db.query(DeviceProtocols).filter(
+            DeviceProtocols.device_id == device_id,
+            DeviceProtocols.protocol_id == protocol_id
+        ).first()
+
+        if existing_link:
+            raise ValueError("Device is already linked with protocol")
+
+        device_protocol = DeviceProtocols(device_id=device_id, protocol_id=protocol_id)
+
+        self.create(device_protocol)
+
+    def unlink(self, device_id: int, protocol_id: int):
+        existing_link = self.db.query(DeviceProtocols).filter(
+            DeviceProtocols.device_id == device_id,
+            DeviceProtocols.protocol_id == protocol_id
+        ).first()
+
+        if not existing_link:
+            raise ValueError("Device is not linked with protocol")
+
+        self.delete(existing_link.id)
+
---
+++ database/services/protocols_service.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+
+from database.models.models import Protocols
+
+class ProtocolService:
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def get_all(self):
+        return self.db.query(Protocols).all()
+
+    def get_by_id(self, id: int):
+        return self.db.query(Protocols).filter(Protocols.id == id).first()
+
+    def get_by_name(self, name: str):
+        return self.db.query(Protocols).filter(Protocols.name == name).first()
+
+    def create(self, data: dict):
+        protocol = Protocols(**data)
+        self.db.add(protocol)
+        self.db.commit()
+        self.db.refresh(protocol)
+        return protocol
+
+    def delete(self, protocol: Protocols):
+        self.db.delete(protocol)
+        self.db.commit()
+
+    def delete_by_id(self, id: int):
+        protocol = self.get_by_id(id)
+        self.delete(protocol)
+    
+    def delete_by_name(self, name: str):
+        protocol = self.get_by_name(name)
+        self.delete(protocol)
+
+    
