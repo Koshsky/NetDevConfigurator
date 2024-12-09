72a84441bcfa10b3ae2c5644818463a28fd1673b
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-09 15:47:31 +0300

    for mes2428

--- modules/ssh_connect.py
+++ modules/ssh_connect.py
@@ -3,11 +3,9 @@ import time
 
 
 def mes_on_open(cls):
-
-    time.sleep(0.25)
     cls.channel.write(cls.auth_password)
     cls.channel.send_return()
-    print(cls.channel.read().decode('utf-8'))
+    cls.send_command("set cli pagination off")  # important
     print("mes_on_open finished")
 
 def mes_on_close(cls):
@@ -21,22 +19,12 @@ mes_config = {
     "auth_password": "MVS_admin",
     "on_open": mes_on_open,
     "on_close": mes_on_close,
-    "comms_prompt_pattern": r"^(\\n)?console(\(.+\))?[#>]\s*$",
+    "comms_prompt_pattern": r"^(\\n)?[a-z0-9.\-_@/:]{1,63}[#>]\s*$",  # 
     "ssh_config_file": "~/NetDevConfigurator/modules/my_ssh_config",
 }
 
-def send_command(ssh, command):
-    ssh.channel.write(command)
-    time.sleep(1)
-
-    response = ssh.channel.read().decode('utf-8')
-
-    return response
-
 if __name__ == "__main__":
     with GenericDriver(**mes_config) as ssh:
-        while True:
-            cmd = input()
-            output = send_command(ssh, cmd)
-            print(output)
-
+        print(ssh.get_prompt())
+        output = ssh.send_command("show running-config")
+        print(output.result)
