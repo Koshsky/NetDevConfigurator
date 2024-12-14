1f67056f4681cb464ff5aa9fd8a5ab1a58cf4ac0
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-15 01:09:31 +0300

    simplify delete button handlers

--- cmd/db_manager/tabs/delete_tab.py
+++ cmd/db_manager/tabs/delete_tab.py
@@ -1,41 +1,19 @@
 from internal.db_app import BaseTab, error_handler
 
-
 class DeleteTab(BaseTab):
     def create_widgets(self):
-        self.create_block("company", {"name":None}, ("delete", self.delete_company))
-        self.create_block("family", {"name":None}, ("delete", self.delete_family))
-        self.create_block("device", {"name":None}, ("delete", self.delete_device))
-        self.create_block("firmware", {"name":None}, ("delete", self.delete_firmware))
-        self.create_block("protocol", {"name":None}, ("delete", self.delete_protocol))
+        entities = ["company", "family", "device", "firmware", "protocol"]
+        for entity in entities:
+            self.create_block(entity, {"name": None}, ("delete", lambda e=entity: self.delete_entity(e)))
+
         self.create_feedback_area()
 
     @error_handler
-    def delete_protocol(self):
-        protocol = self.check_protocol_name(self.fields["protocol"]["name"].get())
-        self.app.entity_services['protocol'].delete(protocol)
-        self.display_feedback("Successfully deleted from the protocols table.")
+    def delete_entity(self, entity_type):
+        check_method = getattr(self, f"check_{entity_type}_name")
+        delete_method = self.app.entity_services[entity_type].delete
 
-    @error_handler
-    def delete_family(self):
-        family = self.check_family_name(self.fields["family"]["name"].get())
-        self.app.entity_services['family'].delete(family)
-        self.display_feedback("Successfully deleted from the families table.")
+        entity = check_method(self.fields[entity_type]["name"].get())
+        delete_method(entity)
+        self.display_feedback(f"Successfully deleted from the {entity_type}s table.")
 
-    @error_handler
-    def delete_company(self):
-        company = self.check_company_name(self.fields["company"]["name"].get())
-        self.app.entity_services["company"].delete(company)
-        self.display_feedback("Successfully deleted from the companies table.")
-        
-    @error_handler
-    def delete_firmware(self):
-        firmware = self.check_firmware_name(self.fields["firmware"]["name"].get())
-        self.app.entity_services["firmware"].delete(firmware)
-        self.display_feedback("Successfully deleted from the firmwares table.")
-
-    @error_handler
-    def delete_device(self):
-        device = self.check_device_name(self.fields["device"]["name"].get())
-        self.app.entity_services["device"].delete(device)
-        self.display_feedback("Successfully deleted from the devices table.")
