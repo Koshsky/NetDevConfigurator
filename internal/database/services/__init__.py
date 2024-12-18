0a5d6ad49c36517ae2955a02a175bb2fe96d9791
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-18 20:55:31 +0300

    mv device services to new folder

--- internal/database/services/__init__.py
+++ internal/database/services/__init__.py
@@ -1,12 +1,12 @@
 # services/__init__.py
 
 from .company_service import CompanyService
-from .device_service import DeviceService
+from .device_services.device_service import DeviceService
 from .firmware_service import FirmwareService, determine_firmware_type
 from .protocol_service import ProtocolService
 from .port_service import PortService
-from .device_firmware_service import DeviceFirmwareService
-from .device_protocol_service import DeviceProtocolService
-from .device_port_service import DevicePortService
+from .device_services.device_firmware_service import DeviceFirmwareService
+from .device_services.device_protocol_service import DeviceProtocolService
+from .device_services.device_port_service import DevicePortService
 from .family_service import FamilyService
 from .template_service import TemplateService
