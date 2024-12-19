71b405feebcac428708ca403d6bc9af22cfb2553
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-19 20:37:41 +0300

    fix import decorator

--- internal/db_app/__init__.py
+++ internal/db_app/__init__.py
@@ -2,3 +2,4 @@ from .database_app import DatabaseApp
 from .connection_tab import ConnectionTab
 from .base_tab import BaseTab
 from .exceptions import RetrievalError
+from .decorators import error_handler
