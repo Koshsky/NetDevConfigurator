8d737871df85c9eca61c08005a1258114c322816
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-01-15 15:21:45 +0300

    -

--- src/modules/ssh/handlers/eltex/mes23xx35xx_handler.py
+++ src/modules/ssh/handlers/eltex/mes23xx35xx_handler.py
@@ -3,7 +3,7 @@ from ..base_handler import BaseHandler, handle_device_open
 from scrapli.driver import GenericDriver
 
 
-class MES23xx35xxHandler(BaseHandler):
+class MES23xx35xxHandler(BaseHandler):  # + mes3300
     def on_open(self, cls: GenericDriver) -> None:
         handle_device_open(
             cls, ["terminal datadump", "terminal width 0", "terminal no prompt"]
