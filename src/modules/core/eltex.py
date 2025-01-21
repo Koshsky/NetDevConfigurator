98c1e89f5f39215fc06aca6c9f94c909f73f25a0
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-01-21 22:42:04 +0300

    -

--- src/modules/core/eltex.py
+++ src/modules/core/eltex.py
@@ -5,7 +5,9 @@ class BaseMES:
     comms_prompt_pattern = r"^(\n)?[a-z0-9_]+[>#\$]\s*$"
     success_signs = {"succeeded", "successful", "success"}
 
-    update_startup_config = "copy tftp://{0}/{1}/{2} startup-config"
+    update_startup_config = (
+        f"copy tftp://{config['tftp-server']['address']}/" + "tmp/{0} startup-config"
+    )
     show_run = "show running-config"
     base_configure = [
         "configure terminal",
@@ -21,13 +23,17 @@ class BaseMES:
 
 class MES14xx24xx34xx37xx(BaseMES):
     open_sequence = ["set cli pagination off"]
-    load_boot = "copy tftp://{0}/{1}/{2} boot"
-    load_firmware = "copy tftp://{0}/{1}/{2} image"
+    load_boot = f"copy tftp://{config['tftp-server']['address']}/" + "firmware/{0} boot"
+    load_firmware = (
+        f"copy tftp://{config['tftp-server']['address']}/" + "firmware/{0} image"
+    )
 
 
 class MES23xx33xx35xx36xx53xx5400(BaseMES):
     open_sequence = ["terminal datadump", "terminal width 0", "terminal no prompt"]
-    load_boot = "boot system tftp://{0}/{1}/{2}"
+    load_boot = (
+        f"boot system tftp://{config['tftp-server']['address']}/" + "firmware/{0}"
+    )
 
 
 class MES11xx21xx20xx31xx(BaseMES):
--- src/modules/ssh/ssh_driver.py
+++ src/modules/ssh/ssh_driver.py
@@ -11,6 +11,9 @@ class SSHDriver(SSHDriverBase):
             resp.result for resp in self.send_commands(self.core.base_configure)
         )
 
+    def update_startup_config(self, filename):
+        return self.send_command(self.core.update_startup_config(filename)).result
+
     def show_bootvar(self):
         return self.send_command(self.core.show_bootvar).result
 
@@ -21,26 +24,18 @@ class SSHDriver(SSHDriverBase):
         filename = find_most_recent_file(
             f"{self.TFTP_FOLDER}/{self.FIRMWARE_FOLDER}", self.device["pattern"]["boot"]
         )
-        return self.send_command(
-            self.core.load_boot.format(self.tftp_server, self.TMP_FOLDER, filename)
-        ).result
+        return self.send_command(self.core.load_boot.format(filename)).result
 
     def update_uboot(self):
         filename = find_most_recent_file(
             f"{self.TFTP_FOLDER}/{self.FIRMWARE_FOLDER}",
             self.device["pattern"]["uboot"],
         )
-        return self.send_command(
-            self.core.load_uboot.format(self.tftp_server, self.TMP_FOLDER, filename)
-        ).result
+        return self.send_command(self.core.load_uboot.format(filename)).result
 
     def update_firmware(self):
         filename = find_most_recent_file(
             f"{self.TFTP_FOLDER}/{self.FIRMWARE_FOLDER}",
             self.device["pattern"]["firmware"],
         )
-        return self.send_command(
-            self.core.load_firmware.format(
-                self.tftp_server, self.FIRMWARE_FOLDER, filename
-            )
-        ).result
+        return self.send_command(self.core.load_firmware.format(filename)).result
