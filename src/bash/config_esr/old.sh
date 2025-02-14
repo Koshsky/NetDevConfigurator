67327df458fdc1ff05e321269b617db13cb8671f
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-02-14 04:04:20 +0300

    тщательно пересмотрел изначальный make_config.sh

--- src/bash/config_esr/old.sh
+++ src/bash/config_esr/old.sh
@@ -4,220 +4,115 @@
 
 lang="bash"
 
-DIR=$(dirname ${BASH_SOURCE})
-LOGS="$DIR/config.log"
-> $LOGS
+parse_config () {
+	COUNT=$(wc -l < $1)
+	for (( i=1;i<=$COUNT;i++ ))
+	do
+		LINE=$(head -n $i $1| tail -n  1)
+		INF_LINE=$(echo $LINE | cut -d '-' -f 1)
+		PAR=$(echo $INF_LINE | cut -d ':' -f 1)
+		VOL=$(echo $INF_LINE | cut -d ':' -f 2)
+		PARAMS[$PAR]=$VOL
+	done
 
-log_message() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOGS"
 }
 
 parse_conf () {
 	cat "$2" | sed -n "/<$1>/,/<@$1>/p"| tail -n +2|head -n -1| cut -f2
 }
 
-
-replace() {
-    if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-        log_message "replace; Ошибка: Необходимые аргументы не указаны. Использование: replace <шаблон> <замена> <файл>"
-        return 1
-    fi
-    if [[ ! -f "$3" ]]; then
-        log_message "replace; Ошибка: Файл '$3' не существует."
-        return 1
-    fi
-    if [[ ! -w "$3" ]]; then
-        log_message "replace; Ошибка: Нет прав на запись в файл '$3'."
-        return 1
-    fi
-
-    log_message "Заменяем <$1> на <$2> в файле '$3'"
-    if sed -ri "s/<$1>/$2/g" "$3"; then
-        log_message "replace; Успех: Замена завершена."
-    else
-        log_message "replace; Ошибка: Не удалось выполнить замену в файле '$3'."
-        return 1
-    fi
-}
-replace_multi() {
-    if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
-        log_message "replace_multi; Ошибка: Необходимые аргументы не указаны. Использование: replace_multi <шаблон> <файл> <файл_для_вставки> <режим>"
-        return 1
-    fi
-    if [[ ! -f "$2" ]]; then
-        log_message "replace_multi; Ошибка: Файл '$2' не существует."
-        return 1
-    fi
-    if [[ ! -f "$3" ]]; then
-        log_message "replace_multi; Ошибка: Файл для вставки '$3' не существует."
-        return 1
-    fi
-    if [[ "$4" -ne 1 && "$4" -ne 2 ]]; then
-        log_message "replace_multi; Ошибка: Неверный режим '$4'. Допустимые значения: 1 или 2."
-        return 1
-    fi
-
-    # Поиск строк с шаблоном
-    local STR_NUM=$(grep -n "<$1>" $2 | cut -f1 -d ":")
-    if [[ -z "$STR_NUM" ]]; then
-        log_message "replace_multi; Ошибка: Шаблон '<$1>' не найден в файле '$2'."
-        return 1
-    fi
-
-    if [ $4 -eq 1 ]; then
-        correct_rule $1 $2 2
-        STR_NUM=$((STR_NUM-1))
-    elif [ $4 -eq 2 ]; then
-        correct_rule $1 $2 1
-    fi
-
-    if sed -i "$STR_NUM r $3" $2; then  # TODO: для чего тут r?
-        log_message "replace_multi; Успех: Вставка из '$3' в файл '$2' на строку $STR_NUM завершена."
-    else
-        log_message "replace_multi; Ошибка: Не удалось выполнить вставку из '$3' в файл '$2'."
-        return 1
-    fi
-}
-correct_rule() {
-    if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-        log_message "correct_rule; Ошибка: Необходимые аргументы не указаны. Использование: correct_rule <шаблон> <файл> <режим>"
-        return 1
-    fi
-    if [[ ! -f "$2" ]]; then
-        log_message "correct_rule; Ошибка: Файл '$2' не существует."
-        return 1
-    fi
-    if [[ "$3" -ne 1 && "$3" -ne 2 ]]; then
-        log_message "correct_rule; Ошибка: Неверный режим '$3'. Допустимые значения: 1 или 2."
-        return 1
-    fi
-
-    # Выполнение удаления строк в зависимости от режима
-    if [ $3 -eq 1 ]; then
-        if sed -i "/<$1>/d" $2 && sed -i "/<@$1>/d" $2; then
-            log_message "correct_rule; Успех: Удалены строки с шаблоном '<$1>' и '<@$1>' из файла '$2'."
-        else
-            log_message "correct_rule; Ошибка: Не удалось удалить строки с шаблоном '<$1>' и '<@$1>' из файла '$2'."
-            return 1
-        fi
-    elif [ $3 -eq 2 ]; then
-        if sed -i "/<$1>/,/<@$1>/d" "$2"; then
-            log_message "correct_rule; Успех: Удалены строки с шаблоном '<$1>' до '<@$1>' из файла '$2'."
-        else
-            log_message "correct_rule; Ошибка: Не удалось удалить строки с шаблоном '<$1>' до '<@$1>' из файла '$2'."
-            return 1
-        fi
-    fi
-}
-count_items() {
-    if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-        log_message "count_items; Ошибка: Необходимые аргументы не указаны. Использование: count_items <конфигурация> <файл> <количество> [дополнительное_число]"
-        return 1
-    fi
-    if [[ ! -f "$1" ]]; then
-        log_message "count_items; Ошибка: Конфигурационный файл '$1' не существует."
-        return 1
-    fi
-    if [[ ! -f "$2" ]]; then
-        log_message "count_items; Ошибка: Файл '$2' не существует."
-        return 1
-    fi
-    if [[ ! -d "$DIR/tmp" ]]; then
-        log_message "count_items; Ошибка: Директория '$DIR/tmp' не существует."
-        return 1
-    fi
-
-    # Парсинг строки конфигурации
-    local PARSE_STR=$(parse_conf $1 $2)
-    local TMP_STR=""
-    local FIN_STR=""
-
-    # Цикл для подсчета элементов
-    for ((i=1; i<=$3; i++)); do
-        declare -n FIN_STR_ITER=FIN_STR
-        TMP_STR=$(echo "$PARSE_STR" | tr ! $i)
-
-        if [[ ! -z "$4" ]]; then
-            TMP_NUM=$(( ${i}+${4} ))  # TODO: TMP_NUM=$((i + $4))
-            TMP_STR=$(echo "$TMP_STR" | tr ? $TMP_NUM)
-        fi
-
-        FIN_STR_ITER+=$(echo "$TMP_STR")$'\n'
-    done
-
-    # Запись результата во временный файл
-    echo "$FIN_STR" | head -n -1 > "$DIR/tmp/tmpstr"
-
-    # Выполнение замены с обработкой ошибок
-    if ! replace_multi $1 $2 $DIR/tmp/tmpstr 1; then
-        log_message "count_items; Ошибка: Не удалось выполнить замену в файле '$2'."
-        return 1
-    fi
-
-    log_message "count_items; Успех: Элементы успешно подсчитаны и заменены в файле '$2'."
+replace () {
+	sed -ri "s/<$1>/$2/g" $3
 }
 
-correct_model="([123]{1})"
-correct_other="([12]{1})"
+replace_multi () {
+	local STR_NUM=$(grep -n "<$1>" $2 | cut -f1 -d ":")
+	if [ $4 -eq 1 ]; then
+		correct_rule $1 $2 2
+		STR_NUM=$((STR_NUM-1))
+	elif [ $4 -eq 2 ]; then
+		correct_rule $1 $2 1
+	fi
+	sed -i "$STR_NUM r $3" $2
+}
 
-error_messages=()
+correct_rule () {
+	if [ $3 -eq "1" ]; then
+		sed -i "/<$1>/d" $2
+		sed -i "/<@$1>/d" $2
+	elif  [ $3 -eq "2" ]; then
+		sed -i "/<$1>/,/<@$1>/d" $2
+	fi
+}
 
-if ! [[ "$VERS" =~ $correct_other ]]; then
-    error_messages+=("VERS: некорректное значение (ожидалось 1 или 2)")
-fi
+count_items () {
+	local PARSE_STR=$( parse_conf $1 $2 )
+	local TMP_STR=""
+	local FIN_STR=""
+	for ((i=1;i<=$3;i++))
+	do
+		declare -n FIN_STR_ITER=FIN_STR
+		TMP_STR=$(echo "$PARSE_STR"| tr ! $i)
+		if [[ ! -z "$4" ]]; then
+		  TMP_NUM=$(( ${i}+${4} ))
+		  TMP_STR=$(echo "$TMP_STR"| tr ? $TMP_NUM)
+		fi
+		FIN_STR_ITER+=$(echo "$TMP_STR")$'\n'
+	done
+	echo "$FIN_STR"| head -n -1 > $DIR/tmp/tmpstr
+	replace_multi $1 $2 $DIR/tmp/tmpstr 1
+}
 
-if ! [[ "$TYPE_COMPLEX" =~ $correct_other ]]; then
-    error_messages+=("TYPE_COMPLEX: некорректное значение (ожидалось 1 или 2)")
-fi
+ROOT_DIR=$PWD
+DIR=$(dirname ${BASH_SOURCE})
 
+declare -A PARAMS
+parse_config "$ROOT_DIR/env.param"
 
-if ! [[ "$MODEL" =~ $correct_model ]]; then
-    error_messages+=("MODEL: некорректное значение (ожидалось 1, 2 или 3)")
-fi
-
-if ! [[ "$VPN" =~ $correct_other ]]; then
-    error_messages+=("VPN: некорректное значение (ожидалось 1 или 2)")
-fi
-
-if ! [[ "$TELEPORT" =~ $correct_other ]]; then
-    error_messages+=("TELEPORT: некорректное значение (ожидалось 1 или 2)")
-fi
-
-if ! [[ "$RAISA" =~ $correct_other ]]; then
-    error_messages+=("RAISA: некорректное значение (ожидалось 1 или 2)")
-fi
-
-if ! [[ "$TRUECONF" =~ $correct_other ]]; then
-    error_messages+=("TRUECONF: некорректное значение (ожидалось 1 или 2)")
-fi
-
-if ! [[ "$TRUEROOM" =~ $correct_other ]]; then
-    error_messages+=("TRUEROOM: некорректное значение (ожидалось 1 или 2)")
-fi
-
-if [ ${#error_messages[@]} -eq 0 ]; then
-    log_message "Формируется файл конфигурации..."
+CERT=${PARAMS["CERT"]}
+PUBLIC_IP=${PARAMS["PUBLIC_IP"]}
+PUBLIC_MASK=${PARAMS["PUBLIC_MASK"]}
+GW=${PARAMS["GW"]}
+MODEL=${PARAMS["MODEL"]}
+VERS=2
+TYPE_COMPLEX=${PARAMS["TYPE_COMPLEX"]}
+if [ $TYPE_COMPLEX -eq 1 ]; then
+	PH_COUNT=${PARAMS["PH_COUNT"]}
+	STREAM_COUNT=${PARAMS["STREAM_COUNT"]}
 else
-    log_message "Некорректные ответы:"
-    for message in "${error_messages[@]}"; do
-        log_message " - $message"
-    done
-    exit 0
-fi
-
-if [ $TYPE_COMPLEX -eq 2 ]; then
 	PH_COUNT=1
 	STREAM_COUNT=1
 fi
-if [ $TRUECONF -eq 2 ]; then
+VPN=${PARAMS["VPN"]}
+TELEPORT=${PARAMS["TELEPORT"]}
+RAISA=${PARAMS["RAISA"]}
+if [ $RAISA -eq 1 ]; then
+	RAISA_IP=${PARAMS["RAISA_IP"]}
+fi
+TRUECONF=${PARAMS["TRUECONF"]}
+if [ $TRUECONF -eq 1 ]; then
+	TRUEROOM=${PARAMS["TRUEROOM"]}
+else
 	TRUEROOM=2
 fi
-
-if [ ! -d $DIR/tmp ]; then
-    mkdir -p $DIR/tmp
+if [ $TRUEROOM -eq 1 ]; then
+	TRUEROOM_COUNT=${PARAMS["TRUEROOM_COUNT"]}
+	declare -a TRUEROOM_IP
+	for ((i=1;i<=$TRUEROOM_COUNT;i++))
+	do
+		TRUEROOM_IP[$i] = ${PARAMS["TRUEROOM_IP$i"]}
+	done
 fi
-rm -rf $DIR/tmp*
 
+correct="([12]{1})"
+if [[ "$VERS" =~ $correct && "$MODEL" =~ $correct && "$VPN" =~ $correct && "$TELEPORT" =~ $correct && "$RAISA" =~ $correct && "$TRUECONF" =~ $correct && "$TRUEROOM" =~ $correct ]]; then
+	echo "Формируется файл конфигурации..."
+else
+	echo "Некорректные ответы"
+	exit 0
+fi
+
+mkdir $DIR/tmp
 touch $DIR/tmp/tmpstr
 
 cat $DIR/templates/main.txt| col -b > $DIR/tmp/main
@@ -448,7 +343,8 @@ fi
 replace "pub_msk" $PUBLIC_MASK "$DIR/tmp/main"
 replace "gate_ip" $GW "$DIR/tmp/main"
 FIN_CONFIG=$( cat $DIR/tmp/main )
-
+rm -R $DIR/tmp
 touch $DIR/config.cfg
 echo "$FIN_CONFIG"| tail -n +2 > $DIR/config.cfg
-# rm -R $DIR/tmp
+touch $DIR/tmp
+echo "$PUBLIC_IP" >> $DIR/tmp
