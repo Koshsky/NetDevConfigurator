0a0a821325d0f6963cbcad79c3d01ccbdc378fc0
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-02-20 18:13:41 +0300

    -

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,10 +3,10 @@ import logging.config
 
 import yaml
 
-LOGGING = yaml.safe_load(open("./src/config/logging_config.yml"))
+LOGGING = yaml.safe_load(open("./src/config/logging_config.yaml"))
 logging.config.dictConfig(LOGGING)
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 logger = logging.getLogger("config")
 
-config = yaml.safe_load(open("./src/config/config.yml"))
-config["router"] = yaml.safe_load(open("./src/config/router.yml"))
+config = yaml.safe_load(open("./src/config/config.yaml"))
+config["router"] = yaml.safe_load(open("./src/config/router.yaml"))
