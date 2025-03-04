5de9c5e849680aa8fd53afea4c48325ae95ffd6f
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-03-04 19:46:33 +0300

    new running config im mock-driver

--- src/drivers/mock/mock_driver.py
+++ src/drivers/mock/mock_driver.py
@@ -29,18 +29,160 @@ class MockDriver:
 
     def show_run(self):
         self.send_command(self.core.show_run)
-        return f"""
-{self.core.comment_symbol} header line 1
-{self.core.comment_symbol} header line 2
-
-hostname pup-123
-
-interface 1
-    be
+        return f"""{self.core.comment_symbol}Building configuration...
+{self.core.comment_symbol}ISS config ver. 9; SW ver. 10.3.6.6 (728c49ab) for MES2424 rev.B. Do not remove or edit this line
 !
-interface 2
-    be
+hostname "rami-mes2424-tsh2"
+debug-logging log-path flash:/mnt/
+dump core-file flash:/mnt/
 !
+no spanning-tree
+!
+vlan 2-4
+  vlan active
+!
+clock time source ntp
+!
+switching-mode cut-through
+!
+username mvsadmin password encrypted zbICg2SPBsvT1IfFwRpH3Q== privilege 15
+username admin_rec password encrypted Ts6Wa2/X/ebD/HnwyJC5NQ== privilege 15
+!
+vlan 2
+  name "VIDEO"
+!
+vlan 3
+  name "DATA"
+!
+vlan 4
+  name "MGMT"
+!
+interface vlan 4
+  ip address 10.4.2.11 255.255.0.0
+!
+interface gigabitethernet 0/1
+  switchport general allowed vlan add 2
+  switchport general allowed vlan add 3 untagged
+  switchport forbidden default-vlan
+  switchport general pvid 3
+!
+interface gigabitethernet 0/2
+  switchport general allowed vlan add 2
+  switchport general allowed vlan add 3 untagged
+  switchport forbidden default-vlan
+  switchport general pvid 3
+!
+interface gigabitethernet 0/3
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/4
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/5
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/6
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/7
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/8
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/9
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/10
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/11
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/12
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/13
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/14
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/15
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/16
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/17
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/18
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/19
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/20
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/21
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/22
+  switchport mode access
+  switchport access vlan 3
+!
+interface gigabitethernet 0/23
+  switchport mode access
+  switchport access vlan 4
+!
+interface gigabitethernet 0/24
+  switchport mode access
+  switchport access vlan 4
+!
+interface tengigabitethernet 0/1
+  switchport general allowed vlan add 2
+  switchport forbidden default-vlan
+!
+interface tengigabitethernet 0/2
+  switchport general allowed vlan add 2
+  switchport forbidden default-vlan
+!
+interface tengigabitethernet 0/3
+  switchport general allowed vlan add 3-4
+  switchport forbidden default-vlan
+!
+interface tengigabitethernet 0/4
+  switchport general allowed vlan add 3-4
+  switchport forbidden default-vlan
+!
+ip route 0.0.0.0  0.0.0.0 10.4.0.254
+!
+no feature telnet
+!
+ztp disable
+end
+
 """
 
     def get_header(self):
