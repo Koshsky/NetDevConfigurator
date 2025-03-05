a73283286a21568b1da280e8e24a20913efd60d7
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-03-05 21:03:04 +0300

    refactor network utils

--- src/utils/network.py
+++ src/utils/network.py
@@ -1,38 +1,71 @@
 import ipaddress
+import logging
+from typing import Callable, Optional
 
 from scapy.all import ARP, Ether, srp
-import logging
 
 logger = logging.getLogger("utils")
 
 
-# TODO: НЕВАЖНО set up network interface with given netplan configuration
 def set_up_dev(dev: str):
-    pass
+    """Sets up the given network interface using Netplan."""
+    raise NotImplementedError("Setting up network interface is not yet implemented.")
 
 
 def scan_network(network):
-    arp = ARP(pdst=network)
+    """
+    Scans the given network for active IP addresses using ARP.
+
+    Args:
+        network: The network to scan (e.g., "192.168.1.0/24").
+
+    Returns:
+        A sorted list of active IP addresses (strings).
+    """
+    try:
+        arp = ARP(pdst=network)
+    except Exception as e:
+        logger.error("Error scanning network: %s", e)
+        return []
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether / arp
 
     result = srp(packet, timeout=2, verbose=0)[0]
 
-    active_ips = [received.psrc for sent, received in result]
-    active_ips.sort(key=lambda x: int(x.split(".")[-1]))
-    return active_ips
+    return sorted(
+        [received.psrc for sent, received in result],
+        key=lambda x: int(x.split(".")[-1]),
+    )
 
 
-def find_available_ip(network, filter=None):
-    logger.info("Searching for available IP-address in %s...", network)
+def find_available_ip(
+    network: str, filter_func: Optional[Callable[[str], bool]] = None
+) -> Optional[str]:
+    """
+    Finds an available IP address in the given network.
+
+    Searches for an available IP address in the specified network, optionally filtering
+    the results using a provided function.
+
+    Args:
+        network: The network to scan (e.g., "192.168.1.0/24").
+        filter_func: An optional function that takes an IP address string as input
+            and returns True if the IP should be considered available, False otherwise.
+
+    Returns:
+        An available IP address as a string, or None if no available IP is found.
+    """
+    logger.info("Searching for available IP address in %s...", network)
     net = ipaddress.IPv4Network(network, strict=False)
     active_ips = scan_network(network)
     for ip in net.hosts():
-        if str(ip) not in active_ips:
-            if filter is not None and not filter(ip):
+        ip_str = str(ip)
+        if ip_str not in active_ips:
+            if filter_func is not None and not filter_func(ip_str):
                 continue
-            logger.info("Find available IP-address: %s", ip)
-            return str(ip)
+            logger.info("Found available IP address: %s", ip_str)
+            return ip_str
+    return None
 
 
 if __name__ == "__main__":
