ac4999569ed1a0416e674774ef593190ae2a0453
Shmonov Matvey Vasilievich <meology77@gmail.com>
2025-03-06 03:23:14 +0300

    docs

--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import logging
 
 from gui.base_app import App
 from gui.tabs.manager import (
@@ -10,13 +11,28 @@ from gui.tabs.manager import (
     TemplateTab,
 )
 
+logger = logging.getLogger(__name__)
+
 
 class DBManagerApp(App):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """Main application class for the database manager."""
+
+    def __init__(self, master: tk.Tk, title: str, *args, **kwargs) -> None:
+        """Initialize the database manager application.
+
+        Args:
+            master: The root Tkinter window.
+            title: The title of the application window.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+        """
+        logger.debug("Initializing DBManagerApp")
+        super().__init__(master, title, *args, **kwargs)
         self.tabs["CONNECTION"].on_button_click()
 
-    def create_tabs(self):
+    def create_tabs(self) -> None:
+        """Create and add tabs to the application."""
+        logger.debug("Creating tabs")
         super().create_tabs()
         self.create_tab(AddTab, "ADD")
         self.create_tab(DeleteTab, "DELETE")
